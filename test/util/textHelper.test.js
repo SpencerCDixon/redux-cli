da73dc516399c41c34ee1921c7e14bc4dd5d5400
Spencer Dixon <spencercdixon@gmail.com>
2016-02-11 17:04:16 -0500

    Assert helper funcs are coloring strings specific colors instead of just adding a color in general

--- test/util/textHelper.test.js
+++ test/util/textHelper.test.js
@@ -1,22 +1,24 @@
-import chalk, { hasColor } from 'chalk';
+import chalk from 'chalk';
 import * as th from 'util/textHelper';
 
 describe('textHelpers', () => {
   describe('#success', () => {
     it('applies green color text', () => {
-      const successString = th.success(
-        'Successfully created something important'
-      );
-      expect(hasColor(successString)).to.be.true;
+      const string = 'Successfully created something important';
+      const successString = th.success(string);
+
+      expect(successString).to.eq(chalk.green(string));
+      expect(successString).to.not.eq(chalk.red(string));
     });
   });
 
   describe('#error', () => {
     it('applies red color to text', () => {
-      const errorString = th.error(
-        'ERROR: something bad happened'
-      );
-      expect(hasColor(errorString)).to.be.true;
+      const string = 'ERROR: something bad happened';
+      const errorString = th.error(string);
+
+      expect(errorString).to.eq(chalk.red(string));
+      expect(errorString).to.not.eq(chalk.green(string));
     });
   });
 });
