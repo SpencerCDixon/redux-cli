a9f4cb310407b0a6a79acd864a2a47dd4fa1751d
Spencer Dixon <spencercdixon@gmail.com>
2016-02-12 09:42:14 -0500

    Adds the other bootstrap style types to text helpers

--- src/util/textHelper.js
+++ src/util/textHelper.js
@@ -1,9 +1,18 @@
 import chalk from 'chalk';
 
+// Bootstrap inspired text color helpers for the command line.
 export const success = (text) => {
   return chalk.green(text);
 };
 
-export const error = (text) => {
+export const danger = (text) => {
   return chalk.red(text);
+};
+
+export const warning = (text) => {
+  return chalk.yellow(text);
+};
+
+export const info = (text) => {
+  return chalk.blue(text);
 };
--- test/util/textHelper.test.js
+++ test/util/textHelper.test.js
@@ -3,7 +3,7 @@ import * as th from 'util/textHelper';
 
 describe('textHelpers', () => {
   describe('#success', () => {
-    it('applies green color text', () => {
+    it('applies green color to text', () => {
       const string = 'Successfully created something important';
       const successString = th.success(string);
 
@@ -12,13 +12,33 @@ describe('textHelpers', () => {
     });
   });
 
-  describe('#error', () => {
+  describe('#danger', () => {
     it('applies red color to text', () => {
       const string = 'ERROR: something bad happened';
-      const errorString = th.error(string);
+      const errorString = th.danger(string);
 
       expect(errorString).to.eq(chalk.red(string));
       expect(errorString).to.not.eq(chalk.green(string));
+    });
+  });
+
+  describe('#warning', () => {
+    it('applies yellow color to text', () => {
+      const string = 'WARNING: are you sure you want this?';
+      const warningString = th.warning(string);
+
+      expect(warningString).to.eq(chalk.yellow(string));
+      expect(warningString).to.not.eq(chalk.red(string));
+    });
+  });
+
+  describe('#info', () => {
+    it('applies blue color to text', () => {
+      const string = 'INFO: this is some useful information';
+      const infoString = th.info(string);
+
+      expect(infoString).to.eq(chalk.blue(string));
+      expect(infoString).to.not.eq(chalk.green(string));
     });
   });
 });
