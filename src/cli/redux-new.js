e27ccac0dde8be28bd7fb030fe7eb5a974d1233d
Spencer Dixon <spencercdixon@gmail.com>
2016-02-22 15:30:33 -0500

    redux new pulls down redux starter kit and inits a git repo

--- src/cli/redux-new.js
+++ src/cli/redux-new.js
@@ -1,6 +1,6 @@
 import commander from 'commander';
-import { error } from '../util/textHelper';
-import { which } from 'shelljs';
+import { error, create } from '../util/textHelper';
+import { which, test, mkdir, cd, exec } from 'shelljs';
 
 class AppGenerator {
   constructor(dirName) {
@@ -9,6 +9,11 @@ class AppGenerator {
 
   run() {
     this.confirmGit();
+    this.confirmDir();
+    this.createDirectory();
+    this.cdDir();
+    this.initGit();
+    this.pullDownKit();
   }
 
   confirmGit() {
@@ -16,6 +21,38 @@ class AppGenerator {
       console.log(error('This script requires you have git installed'));
       process.exit(1);
     }
+  }
+
+  confirmDir() {
+    if (test('-d', this.dirName)) {
+      error(`${this.dirName} directory already exists!  Please choose another name`);
+      process.exit(1);
+    }
+  }
+
+  createDirectory() {
+    mkdir('-p', this.dirName);
+    create('project directory created');
+  }
+
+  cdDir() {
+    cd(this.dirName);
+  }
+
+  initGit() {
+    create('setting up tracking with git...');
+    console.log();
+    exec('git init');
+  }
+
+  pullDownKit() {
+    exec('git pull https://github.com/davezuko/react-redux-starter-kit.git', function(code) {
+      if (code !== 0) {
+        error('Something went wrong... please try again');
+        process.exit(1);
+      }
+      create('Completed... project ready to go!');
+    });
   }
 }
 
