a4b21658bc9328ef2ceab3bbb539e1b5f8b1b8c1
Spencer Dixon <spencercdixon@gmail.com>
2016-02-28 10:25:28 -0500

    Finishes refactoring new command to use updated design

--- src/sub-commands/new.js
+++ src/sub-commands/new.js
@@ -1,7 +1,8 @@
-import { which, rm, exec, pwd } from 'shelljs';
+import { which, rm, exec } from 'shelljs';
 import SubCommand from '../models/sub-command';
 import CreateAndStepIntoDirectory from '../tasks/create-and-step-into-directory';
 import GitPull from '../tasks/git-pull';
+import ProjectSettings from '../models/project-settings';
 
 // eventually allow users to create new projects based on a flag
 // ie. they can create a new react-redux-starter-kit or a new
@@ -18,8 +19,8 @@ class New extends SubCommand {
     this.confirmGit();
     this.createDirTask.run(cliArgs).then(() => {
       this.gitPullTask.run('git@github.com:davezuko/react-redux-starter-kit.git').then(() => {
+        this.createProjectSettings();
         this.resetGitHistory();
-        // this.createProjectSettings();
       });
     });
   }
@@ -36,25 +37,25 @@ class New extends SubCommand {
     this.ui.write('Command used for generating new redux projects');
   }
 
+  // Should maybe prompt user for permission to do this since it's dangerous.
   resetGitHistory() {
-    // Should maybe prompt user for permission to do this since it's dangerous.
     this.ui.writeInfo('Removing the starter kit .git folder');
-    this.ui.writeInfo(pwd());
-    // rm('-rf', '.git');
+    rm('-rf', '.git');
     exec('git init && git add -A && git commit -m"Initial commit"', {silent: true});
     this.ui.writeCreate('Created new .git history for your project');
     this.ui.writeInfo('Congrats! New Redux app ready to go.  CLI generators configured and ready to go');
   }
 
-  // createProjectSettings() {
-    // // All settings for react-redux-starter-kit live in this template so when
-    // // new projects get created users can immediately start using the CLI
-    // const reduxStarterKitTemplate = '../templates/.starterrc';
-    // const settings = new ProjectSettings(reduxStarterKitTemplate);
-    // settings.save();
+  // All settings for react-redux-starter-kit live in this template so when
+  // new projects get created users can immediately start using the CLI
+  createProjectSettings() {
+    this.ui.writeInfo('creating a default .reduxrc for your project');
+    const reduxStarterKitTemplate = '../../templates/.starterrc';
+    const settings = new ProjectSettings(reduxStarterKitTemplate);
+    settings.save();
 
-    // create('.reduxrc with starter kit settings saved.');
-  // }
+    this.ui.writeCreate('.reduxrc with starter kit settings saved.');
+  }
 }
 
 export default New;
